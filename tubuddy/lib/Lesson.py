432aa611b2b19dc1343c7e7f3b782a207ebd7f8c
Aral <a.d.demoor@student.tudelft.nl>
2023-10-18 11:41:39 +0200

    dir: cleanup old scripts

--- tubuddy/lib/Lesson.py
+++
@@ -1,6 +0,0 @@
-class Lesson:
-    """ 
-    Represents a lesson at a given day and time.
-    """
-    
-    def __init__(self,  
--- tubuddy/lib/Test.py
+++
@@ -1,9 +0,0 @@
-class Test:
-    
-    def __init__(self, courseid, name, date, location)
-        self.courseid = courseid
-        self.name = name
-        self.date = date
-        self.location = location
-
-    
